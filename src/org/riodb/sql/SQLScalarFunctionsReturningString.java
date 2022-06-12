18c39a7fe7241142a5a968f185f666ddfd20aae7
th7mo <thimo@th7mo.nl>
2022-06-12 15:59:11 +0200

    add lower function for string:

--- src/org/riodb/sql/SQLScalarFunctionsReturningString.java
+++ src/org/riodb/sql/SQLScalarFunctionsReturningString.java
@@ -1,8 +1,8 @@
 /*
  	Copyright (c) 2021 Lucio D Matos,  www.riodb.org
- 
+
     This file is part of RioDB
-    
+
     RioDB is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -15,17 +15,17 @@
 
     A copy of the GNU General Public License should be found in the root
     directory. If not, see <https://www.gnu.org/licenses/>.
- 
+
 */
 
 /*
  *   Scalar functions that return STRING
- *   
+ *
  *   Instructions to add a new function:
- *   
+ *
  *   1- add the function name (in lower-case) to the array of functions
  *   2- write the function, with its name in lowercase always!
- *   
+ *
  */
 package org.riodb.sql;
 
@@ -90,4 +90,7 @@ public final class SQLScalarFunctionsReturningString {
         return (s == null) ? null : s.toUpperCase();
     }
 
+    public static String lower(String s) {
+        return (s == null) ? null : s.toLowerCase();
+    }
 }
